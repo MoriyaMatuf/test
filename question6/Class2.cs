56bb4a5abb21816dfdafd373b167cb2dd1068518
MoriyaMatuf <Moria6921@gmail.com>
2022-11-17 22:54:36 +0200

    njh

---
+++ question6/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question6
+{
+    class Class2:question_6
+    {
+       public override int add()
+        {
+            return 3;
+        }
+    }
+}
---
+++ question6/question_6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question6
+{
+   public abstract class question_6
+    {
+        public abstract int add();
+     
+        
+        
+
+
+
+    }
+}
